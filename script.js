e97e6b610c5f7144954dd09907baac3d9eb23175
selea <sve5040@gmail.com>
2021-12-29 12:07:12 -0500

    prevent game from start w/o a guess

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+let checkBtn = 'userInputCheckBtn';
+let replayBtn = 'playAgainBtn';
+let userInput = 'userinput';
+let msgToUserUI = document.getElementById('msgToUserDisplay');
+let centerSelection = document.getElementById('centerSelection');
+let userGuess, randNumb;
+
 // function to get random number generator
-let randNumb;
+
 let randomNumb = function () {
 	randNumb = Math.floor(Math.random() * 3);
 	console.log(`the random number is ${randNumb}`);
 };
 
 //function to compare user guess and random number generator
-
 let guessCheck = function (randomNumber, guessfromUser) {
 	let initialScore = Number(document.getElementById('scoreSpan').innerHTML);
 	let currentHighScore = Number(
@@ -18,27 +24,20 @@ let guessCheck = function (randomNumber, guessfromUser) {
 
 	//if statement to check if the arguments are the same
 	if (Number(randomNumber) === Number(guessfromUser)) {
-		document.getElementById(
-			'msgToUserDisplay'
-		).innerHTML = `<i class="fas fa-check-circle fa-2x"></i> Correct Answer`;
+		msgToUserUI.innerHTML = `<i class="fas fa-check-circle fa-2x"></i> Correct Answer🎉`;
 		changeOps = 1;
 		updateScore(initialScore, changeOps);
 		replaceCenterChoice(randomNumber);
 		changeBckgrndColor('green');
-
 		updateHighScore(Number(guessfromUser), currentHighScore);
 	} else if (Number(randomNumber) > Number(guessfromUser)) {
-		document.getElementById(
-			'msgToUserDisplay'
-		).innerHTML = `<i class="fas fa-arrow-down fa-2x"></i> Too low`;
+		msgToUserUI.innerHTML = `<i class="fas fa-arrow-down fa-2x"></i> Too low`;
 		changeOps = -1;
 		updateScore(initialScore, changeOps);
 		replaceCenterChoice(randomNumber);
 		changeBckgrndColor('red');
 	} else {
-		document.getElementById(
-			'msgToUserDisplay'
-		).innerHTML = `<i class="fas fa-arrow-up fa-2x"></i> Too high`;
+		msgToUserUI.innerHTML = `<i class="fas fa-arrow-up fa-2x"></i> Too high`;
 		changeOps = -1;
 		updateScore(initialScore, changeOps);
 		replaceCenterChoice(randomNumber);
@@ -52,28 +51,27 @@ let updateScore = function (initialScore, changeOperation) {
 	document.getElementById('scoreSpan').innerHTML = newScoreToDispay;
 };
 
-//function to render - the question mark replaced with guess
+//function to render number to guess - the question mark replaced with guess
 let replaceCenterChoice = function (theUserGuess) {
-	document.getElementById('centerSelection').innerHTML = theUserGuess;
+	centerSelection.innerHTML = `Number: ${theUserGuess}`;
 };
-//function to change background color
 
+//function to change background color
 let changeBckgrndColor = function (bgcolor) {
 	document.body.style.background = bgcolor;
 };
 
-//function to store the user input
-let checkBtn = 'userInputCheckBtn';
-let replayBtn = 'playAgainBtn';
-let userInput = 'userinput';
-let userGuess;
-
 ///start game on click
-
 document.getElementById(checkBtn).addEventListener('click', function () {
 	randomNumb();
 	userGuess = document.getElementById(userInput).value;
-	guessCheck(randNumb, userGuess);
+	if (!userGuess) {
+		console.log('no number guessed');
+		msgToUserUI.innerHTML = `🛑 No number guessed`;
+	} else {
+		console.log('there is a number');
+		guessCheck(randNumb, userGuess);
+	}
 });
 
 //reset game on click
@@ -82,13 +80,8 @@ document.getElementById(replayBtn).addEventListener('click', function () {
 	document.getElementById(userInput).value = ' ';
 	document.getElementById('scoreSpan').innerHTML = 20;
 	document.getElementById('highscoreSpan').innerHTML = 0;
-
-	document.getElementById(
-		'centerSelection'
-	).innerHTML = `<i class="fas fa-question-circle fa-3x"></i>`;
-	document.getElementById(
-		'msgToUserDisplay'
-	).innerHTML = `<i class="fas fa-chart-bar fa-2x"></i> Start Guessing`;
+	centerSelection.innerHTML = `<i class="fas fa-question-circle fa-3x"></i>`;
+	msgToUserUI.innerHTML = `<i class="fas fa-chart-bar fa-2x"></i> Start Guessing`;
 });
 
 //function to update highsore
